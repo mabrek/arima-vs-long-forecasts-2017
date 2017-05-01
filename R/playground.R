202c7d28043ae00924c02bc71025be652066dd4b
Anton Lebedevich <mabrek@gmail.com>
2017-05-01 14:36:41 +0300

    manual arima

--- R/playground.R
+++ R/playground.R
@@ -14,27 +14,29 @@ shinyplot(nn5, 150)
 multiplot(nn5[, c(1, 26, 89, 110)], vline = last(nn5_train_range))
 
 
-pattern <- c(5, 3, 3, 2, 6, 0, 0)
+pattern <- c(5, 4.7, 4, 4.5, 6, 0, 0)
 long <- rep(pattern, 100)
 long <- long + rnorm(length(long), 0, 0.5)
 long[seq_along(long) %% 7 == 6] <- 0
 long[seq_along(long) %% 7 == 0] <- 0
 
-clean <- rep(pattern1, 4)
+clean <- rep(pattern, 4)
 short <- long[1:28]
 x <- 1:28
 ggplot() + geom_path(aes(x = x, y = clean, color = "red")) +
   geom_path(aes(x = x, y = short, color = "black")) +
   scale_colour_identity() + xlab(NULL) + ylab(NULL) + tm
 
-fit <- auto.arima(long)
+auto.fit <- auto.arima(long, max.p = 28, max.q = 5, max.order = 40, start.p = 1, start.q = 1)
 
-fit <- auto.arima(long, max.p = 28, max.q = 5, max.order = 40, start.p = 1, start.q = 1)
+x <- 1:70
 
-fit <- Arima(long, c(7, 0, 0))
+ggplot() + geom_path(aes(x = x, y = rep(pattern, 10), color = "green")) +
+  geom_path(aes(x = x, y = forecast(auto.fit, h = 70)$mean, color = "blue")) +
+  scale_colour_identity() + ggtitle(as.character(auto.fit)) + xlab(NULL) + ylab(NULL) + tm
 
-fit <- Arima(long, c(14, 0, 1))
+manual.fit <- Arima(long, c(7, 0, 0))
 
-plot(forecast(fit, h = 70))
-plot(fit)
-fit
+ggplot() + geom_path(aes(x = x, y = rep(pattern, 10), color = "green")) +
+  geom_path(aes(x = x, y = forecast(manual.fit, h = 70)$mean, color = "blue")) +
+  scale_colour_identity() + ggtitle(as.character(manual.fit)) + xlab(NULL) + ylab(NULL) + tm
